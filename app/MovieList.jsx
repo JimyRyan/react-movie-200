498ea83560978dca08da1417e795cb2e97fc842a
JimyRyan <JimyRyan@gmail.com>
2016-01-21 00:55:02 +0100

    fix

--- app/MovieList.jsx
+++ app/MovieList.jsx
@@ -6,13 +6,18 @@ var MOVIES_API_URL = "/server/api/movies";
 
 var MovieList = React.createClass({
 
+	// Init the data
 	getInitialState: function() {
-		return {movies: {loading: true, data: []}};
+		return {movies: {loading: false, data: []}};
 	},
 
-	componentWillMount: function() {
+	// Load the movies
+	componentWillMount: function () {
 
-		// Loading
+		// Will display the loading...
+		this.setState({
+			movies: {loading: true}
+		});
 
 		// Make a request for a user with a given ID
 		axios.get(MOVIES_API_URL)
@@ -24,20 +29,21 @@ var MovieList = React.createClass({
 
 			}.bind(this))
 			.catch(function (response) {
-				//console.log(response);
-			});
+				console.log(response);
+		});
 	},
 
 
 	render: function () {
 
-		if (this.state.loading) {
-
+		// While movies is loading, display the spinner and exit
+		if (this.state.movies.loading) {
 			return (
-				<div class="loading">Loading&#8230;</div>
+				<div className="loading">Loading&#8230;</div>
 			)
 		}
 
+		// When the movies are loaded, remove the spiner and display the movies
 		var moviesJson = this.state.movies.data;
 		var movies = moviesJson.map(function (movie) {
       		return <Movie film={movie} />
