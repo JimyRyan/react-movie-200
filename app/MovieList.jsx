a8090f81350da1de4bf6c51a3a32e0d68e2a1f45
JimyRyan <JimyRyan@gmail.com>
2016-01-20 14:26:46 +0100

    mon exo

--- app/MovieList.jsx
+++ app/MovieList.jsx
@@ -1,50 +1,53 @@
 var React = require('react');
 var Movie = require('./Movie.jsx');
+var axios = require('axios');
 
-var MOVIES = [
-  {
-		id: 1,
-		title : "Avatar",
-		category : 'sciencesfiction',
-		releaseYear : "2010",
-		poster : "img/avatar.jpg",
-		directors : "James Cameron",
-		actors : "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang, Michelle Rodriguez",
-		synopsis : "Sur la lointaine planète de Pandora, Jake Sully, un héros malgré lui, " +
-		"se lance dans une quête de rédemption, de découverte, d'amour inattendu, dont l'issue sera un " +
-		"combat héroïque pour sauver toute une civilisation.",
-		rate : 3,
-		lastViewDate : new Date(2013, 3, 1, 12, 4, 50),
-		price : 25.46
-	},
-	{
-		id: 2,
-		title : "Seigneur des Anneaux : La Communauté de l'Anneau",
-		category : 'sciencesfiction',
-		releaseYear : "2001",
-		poster : "img/seigneurdesanneaux1.jpg",
-		directors : "Peter Jackson",
-		actors : "Elijah Wood, Sean Astin, Ian McKellen, Sala Baker, Viggo Mortensen",
-		synopsis : "Frodon le Hobbit hérite de l'Anneau Unique, un instrument de pouvoir absolu" +
-		"qui permettrait à Sauron, le Seigneur des ténèbres, de régner sur la Terre du Milieu." +
-		" Commence alors un vaste périple visant à la destruction de l'objet.",
-		rate : 5,
-		lastViewDate : new Date(2012, 5, 13, 9, 30, 45),
-		price : 34.76
-	}
-];
+var MOVIES_API_URL = "/server/api/movies";
 
 var MovieList = React.createClass({
-  render: function () {
-    var movies = MOVIES.map(function (movie) {
-      return <Movie film={movie} />
-    });
 
-    return (
-      <ul className="thumbnails list-unstyled">
-        {movies}
-      </ul>
-    );
+	getInitialState: function() {
+		return {movies: {loading: true, data: []}};
+	},
+
+	componentWillMount: function() {
+
+		// Loading
+
+		// Make a request for a user with a given ID
+		axios.get(MOVIES_API_URL)
+			.then(function (response) {
+				//console.log(response);
+				this.setState({
+					movies: {loading: false, data: response.data }
+				})
+
+			}.bind(this))
+			.catch(function (response) {
+				//console.log(response);
+			});
+	},
+
+
+	render: function () {
+
+		if (this.state.loading) {
+
+			return (
+				<div class="loading">Loading&#8230;</div>
+			)
+		}
+
+		var moviesJson = this.state.movies.data;
+		var movies = moviesJson.map(function (movie) {
+      		return <Movie film={movie} />
+    	});
+
+    	return (
+      		<ul className="thumbnails list-unstyled">
+        		{movies}
+      		</ul>
+    	);
   }
 });
 
