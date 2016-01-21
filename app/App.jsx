f52a673f0dff875068ed42dc352ca52d011b9949
JimyRyan <JimyRyan@gmail.com>
2016-01-21 09:32:49 +0100

    mon exo

--- app/App.jsx
+++ app/App.jsx
@@ -6,62 +6,79 @@ var SearchBar = require('./SearchBar.jsx');
 var MovieAPI = require('./api/MovieAPI');
 
 var App = React.createClass({
-  getInitialState: function () {
-    return {
-      movies: [],
-      loadingMovies: false,
-      searchKey: ''
-    }
-  },
+    getInitialState: function () {
+        return {
+            movies       : [],
+            loadingMovies: false,
+            searchKey    : ''
+        }
+    },
 
-  componentWillMount: function () {
-    this.setState({
-      loadingMovies: true
-    });
-
-    MovieAPI.getMovieList().then(function (movies) {
-      this.setState({
-        movies: movies,
-        loadingMovies: false
-      })
-    }.bind(this));
-  },
-
-  onMovieDeletion: function (movieId) {
-    MovieAPI.removeMovie(movieId).then(function () {
-      var filteredMovieList = this.state.movies.filter(function (movie) {
-        return movie.id !== movieId;
-      });
-
-      this.setState({
-        movies: filteredMovieList
-      });
-    }.bind(this));
-  },
-
-  onSearch: function (searchKey) {
-    this.setState({
-      searchKey: searchKey
-    });
-  },
-
-  render: function () {
-    var searchKey = this.state.searchKey,
-        displayedMovies = this.state.movies.filter(function (movie) {
-          return movie.title.toLowerCase().match(searchKey.toLowerCase());
+    componentWillMount: function () {
+        this.setState({
+            loadingMovies: true
         });
 
-    return (
-      <div>
-        <Header />
-        <SearchBar onSearch={this.onSearch} />
-        <MovieList
-            movies={displayedMovies}
-            loadingMovies={this.state.loadingMovies}
-            onMovieDeletion={this.onMovieDeletion} />
-      </div>
-    );
-  }
+        MovieAPI.getMovieList().then(function (movies) {
+            this.setState({
+                movies       : movies,
+                loadingMovies: false
+            })
+        }.bind(this));
+    },
+
+    onMovieDeletion: function (movieId) {
+        MovieAPI.removeMovie(movieId).then(function () {
+            var filteredMovieList = this.state.movies.filter(function (movie) {
+                return movie.id !== movieId;
+            });
+
+            this.setState({
+                movies: filteredMovieList
+            });
+        }.bind(this));
+    },
+
+    onSearch: function (searchKey) {
+        this.setState({
+            searchKey: searchKey
+        });
+    },
+
+    /*
+    // OK car app simple, mais sujet a bug si IHM plus complex
+    shouldComponentUpdate: function (nextProps, nextState) {
+
+        var oldSearchKey = this.state.searchKey;
+        var newSearchKey = nextState.searchKey;
+
+        // Changement du mot clef ou actualisation des films
+        if (oldSearchKey !== newSearchKey || this.state.movies !== nextState.movies) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    */
+
+    render: function () {
+        var searchKey       = this.state.searchKey,
+            displayedMovies = this.state.movies.filter(function (movie) {
+                return movie.title.toLowerCase().match(searchKey.toLowerCase());
+            });
+
+        return (
+            <div>
+                <Header />
+                <SearchBar onSearch={this.onSearch}/>
+                <MovieList
+                    movies={displayedMovies}
+                    searchKey={this.state.searchKey}
+                    loadingMovies={this.state.loadingMovies}
+                    onMovieDeletion={this.onMovieDeletion}/>
+            </div>
+        );
+    }
 });
 
 
--- app/MovieList.jsx
+++ app/MovieList.jsx
@@ -2,26 +2,42 @@ var React = require('react');
 var Movie = require('./Movie.jsx');
 
 var MovieList = React.createClass({
-  render: function () {
-    var movies = this.props.movies;
-    var onMovieDeletion = this.props.onMovieDeletion;
-    var moviesTag = movies.map(function (movie) {
-      return <Movie key={movie.id} film={movie} onMovieDeletion={onMovieDeletion} />
-    });
-    var content;
 
-    if (this.props.loadingMovies) {
-      content = <li>Chargement de la liste des films en cours</li>
-    } else {
-      content = moviesTag;
+    // Solution préférable
+    shouldComponentUpdate: function (nextProps, nextState) {
+
+        var oldSearchKey = this.props.searchKey;
+        var newSearchKey = nextState.searchKey;
+
+        // Changement du mot clef ou actualisation des films
+        if (oldSearchKey !== newSearchKey || this.props.movies !== nextState.movies) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    render: function () {
+        var movies = this.props.movies;
+        var onMovieDeletion = this.props.onMovieDeletion;
+        var searchKey = this.props.searchKey;
+        var moviesTag = movies.map(function (movie) {
+            return <Movie key={movie.id} film={movie} onMovieDeletion={onMovieDeletion}/>
+        });
+        var content;
+
+        if (this.props.loadingMovies) {
+            content = <li>Chargement de la liste des films en cours</li>
+        } else {
+            content = moviesTag;
+        }
+
+        return (
+            <ul className="thumbnails list-unstyled">
+                {content}
+            </ul>
+        );
     }
-
-    return (
-      <ul className="thumbnails list-unstyled">
-        {content}
-      </ul>
-    );
-  }
 });
 
 module.exports = MovieList;
